b1866323f36262a8ca901e9e991ad2ac875dd053
Robert Syvarth <robert@duxter.com>
2015-10-02 11:20:52 -0500

    Update the cron

--- application/views.py
+++ application/views.py
@@ -70,6 +70,9 @@ def digest():
 This is a digest email.
 """
     entries = EntryModel.query(EntryModel.timestamp > (datetime.datetime.now() + datetime.timedelta(days=-1))).order(-EntryModel.timestamp)
+    if len(entries) == 0:
+        return ""
+
     for entry in entries:
         body = body + """
     %s
